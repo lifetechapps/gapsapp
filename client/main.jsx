ba8fcd28f6812d8981f1a54f138498cdb4392a83
Randy Lexvold <rlexvold@gmail.com>
2017-02-11 09:27:55 -0800

    continued category sorting

--- client/main.jsx
+++ client/main.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import {Meteor} from 'meteor/meteor';
-import {render} from 'react-dom';
+import React from 'react'
+import {Meteor} from 'meteor/meteor'
+import {render} from 'react-dom'
 import {renderRoutes} from '../imports/startup/client/routes.jsx'
 
 import ons from 'onsenui'
@@ -10,5 +10,5 @@ import 'onsenui/css/onsen-css-components.css'
 Meteor.startup(() => {
   ons.ready(() => {
     render(renderRoutes(), document.getElementById('app'))
-  });
-});
+  })
+})
